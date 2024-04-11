03b2e171ab63b8d0262eee6bdf2fa684b42b8381
Albin <abbelundmark@gmail.com>
2024-04-11 13:25:32 +0200

    1

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { CarProvider } from './context/CarContext'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <CarProvider>
+        <App />
+    </CarProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
